a87fc5977ced983b9c6029792a014b3aa798dd12
Rafael Galdino <88910477+Rafael-GSousa@users.noreply.github.com>
2023-11-13 23:26:05 -0300

    Add desafio aula 05

--- Desafios/Aula05/index.js
+++ Desafios/Aula05/index.js
@@ -1,10 +1,15 @@
-const readline = require('readline-sync')
-const peso = Number(readline.question('Digite seu peso em Kg => '))
-const altura = Number(readline.question('Digite sua altura em m => '))
+const readline = require('readline-sync') //importa a biblioteca readline-sync do NodeJS
+const peso = Number(readline.question('Digite seu peso em Kg => ')) // Entrada do usuário direto no terminal do NodeJS
+const altura = Number(readline.question('Digite sua altura em m => ')) // Entrada do usuário direto no terminal do NodeJS
+
+// Entradas de dados pelo browser
 // const peso = Number(prompt('Digite seu peso em Kg'))
 // const altura = Number(prompt('Digite sua altura em m'))
-const imc = peso / Math.pow(altura, 2)
-let msg = '';
+
+const imc = peso / Math.pow(altura, 2) // cálculo do IMC
+let msg = ''; // variável onde será atribuída a mensagem
+
+// Verificando em qual faixa de peso se enquadra o IMC
 if (imc < 18.5){
   msg = 'Abaixo do peso'
 } else if (imc < 24.9){
@@ -14,4 +19,6 @@ if (imc < 18.5){
 }else {
   msg = 'Obesidade'
 }
+
+// Exibindo o resultado e a mensagem no console
 console.log(`Seu IMC: ${imc.toFixed(2)}\nSua faixa: ${msg}`)
